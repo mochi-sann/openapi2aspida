1f53e87aeb7fff5c57127d19bde22ead77f3a439
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2021-08-10 15:58:32 +0900

    helpを表示するための aliasを追加

--- src/cli.ts
+++ src/cli.ts
@@ -3,12 +3,16 @@ import build from '.'
 
 export const run = (args: string[]) => {
   const argv = minimist(args, {
+    boolean: ['help'],
     string: ['version', 'input', 'config'],
-    alias: { v: 'version', i: 'input', c: 'config' }
+    alias: { v: 'version', i: 'input', c: 'config', h: 'help' }
   })
+  console.log(argv)
 
   argv.version !== undefined
     ? console.log(`v${require('../package.json').version}`)
+    : argv.help
+    ? console.log('helpを表示するところです')
     : argv.input
     ? build({ outputEachDir: true, openapi: { inputFile: argv.input } })
     : build(argv.config)
